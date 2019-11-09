1927377a0a38b837234e2a6630c5d10646ed67db
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-11-09 17:38:07 +0400

    Add node-sass, Loading component, Weather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import Weather from './pages/Weather/Weather';
+import Loading from './components/Loading';
 
 const App: React.FC = () => {
+  const [ loading, setLoading ] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40')
+      .then((response) => response.json())
+      .then((weatherData) => {
+        console.log(weatherData);
+        setLoading(false);
+      })
+  }, []);
+
   return (
     <div className="app">
-      hello
+      {loading ? <Loading /> : <Weather />}
     </div>
   );
 }
