456cd25d31b9c318d70d378b94583d2598041420
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-11-17 21:43:42 +0400

    Configured deployment to github pages

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
   API: {
     weather: {
       method: HttpMethod.GET,
-      url: 'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
+      url: 'https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
     }
   }
 };
