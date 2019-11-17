d28089d94832850dcb342e1092557fff210a0741
SergeiKalachev <Sergei_Kalachev@epam.com>
2019-11-17 21:06:08 +0400

    Updates

---
+++ precache-manifest.51b22037c65210fab78fc9759e3f0859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5431674c92d2c7b862721c2e6ecda364",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "502a0daa3e861c5210e4",
+    "url": "/weather-app/static/css/main.8a55a1d6.chunk.css"
+  },
+  {
+    "revision": "7a5fd3079b3852acba34",
+    "url": "/weather-app/static/js/2.b0b5957c.chunk.js"
+  },
+  {
+    "revision": "502a0daa3e861c5210e4",
+    "url": "/weather-app/static/js/main.ae6db138.chunk.js"
+  },
+  {
+    "revision": "b0ae6103f372a3fc325e",
+    "url": "/weather-app/static/js/runtime-main.9bf36d48.js"
+  }
+]);
